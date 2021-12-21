13746cad2a6c37ea13abe2d3b696a50618735b59
JasonX <jason@none.com>
2021-12-21 23:34:42 +0800

    test

--- client/src/components/Navbar2/General/InfoGuideline/InfoGuideline.js
+++ client/src/components/Navbar2/General/InfoGuideline/InfoGuideline.js
@@ -1,30 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { 
-    InputAdornment,
-    Container,
-    Grow,
-    Typography,
-    Paper,
-    TextField,
-} from "@material-ui/core";
+import { InputAdornment, Container, Grow, Typography, Paper, TextField } from "@material-ui/core";
 import { Search as SearchIcon } from "@material-ui/icons";
 
-
 const InfoGuideline = () => {
-
     // //Table Arrange States
     // const [order, setOrder] = useState("desc");
     // const [orderBy, setOrderBy] = useState("createdAt");
-    
 
     return (
         <Grow in>
             <Container maxWidth={false}>
-            <Typography variant="h5"> All Info and Guidelines</Typography>
-            <div></div>
-            <Paper>
-            
-            </Paper>
+                <Typography variant="h5"> All Info and Guidelines</Typography>
+                <div></div>
+                <Paper></Paper>
             </Container>
         </Grow>
     );
