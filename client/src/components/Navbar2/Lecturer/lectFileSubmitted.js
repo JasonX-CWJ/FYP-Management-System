d20d8395a9a0cecf6ed5e4ba43dd92864e8e0b58
zanafdzl <sya.zanafdzl@gmail.com>
2022-01-16 12:29:56 +0800

    fixed LectProjectDetails

--- client/src/components/Navbar2/Lecturer/lectFileSubmitted.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Container } from "@material-ui/core";
-
-const lectFileSubmitted = () => {
-    return <Container>lectFileSubmitted here</Container>;
-};
-
-export default lectFileSubmitted;
