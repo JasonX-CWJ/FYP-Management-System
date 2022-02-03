126fbbfd1ce3cfc8312ace02cebc9e0d1e17d296
JasonX <jason@none.com>
2022-02-03 20:00:34 +0800

    Initial role support

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import ROLE from "../constants/userRole.js";
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
@@ -6,6 +7,10 @@ const userSchema = mongoose.Schema({
     password: { type: String, required: true },
     id: { type: String },
     // role: { type: String },
+    isFirstLogin: { type: Boolean, default: true },
+    role: { type: String, enum: ROLE, default: ROLE.STUDENT },
+    studentData: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
+    lecturerData: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lecturer" }],
 });
 
 export default mongoose.model("User", userSchema);
