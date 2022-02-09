4009e6f176f72bdae7c8ddfac0d34c79fe258477
JasonX <jason@none.com>
2022-02-09 22:59:51 +0800

    complete meeting card

--- client/src/components/Navbar2/Cards/MeetingsCompletedCard.js
+++ client/src/components/Navbar2/Cards/MeetingsCompletedCard.js
@@ -18,15 +18,32 @@ import {
     TablePagination,
     Toolbar,
     Box,
-    IconButton, Collapse
+    IconButton,
+    Collapse,
 } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import { getLectMeetings } from "../../../actions/Lecturer/LectMeetings";
+import MEETING from "../../../constants/meetingStatus";
+import ROLE from "../../../constants/userRole";
 
 const MeetingsCompletedCard = () => {
+    const dispatch = useDispatch();
+    const studM = useSelector((state) => state.lectMeetings);
+    const user = JSON.parse(localStorage.getItem("profile")); // get current user
+    const [currentId, setCurrentId] = useState(0);
+
+    useEffect(() => {
+        // dispatch(getMeetings());
+        dispatch(getLectMeetings());
+    }, [currentId, dispatch]);
+    var newArray = studM.filter(function (row) {
+        return row?.studentID?._id === user?.result?.studentData?._id && row?.status === MEETING.COMPLETE;
+    });
+
     return (
-        <Paper align="center" style={{ margin: "16px 0px", padding: 8, }}>
-                <Typography variant="h6">Meetings Completed</Typography>
-                <Typography variant="h3">5</Typography>
-                change the 5 above into the number of meetings actually completed
+        <Paper align="center" style={{ margin: "16px 0px", padding: 8 }}>
+            <Typography variant="h6">Meetings Completed</Typography>
+            <Typography variant="h3">{newArray.length}</Typography>
         </Paper>
     );
 };
